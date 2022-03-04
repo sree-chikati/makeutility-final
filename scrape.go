61840b1bfd657e4d04cf5853686d672b0b4db7b5
Sreenvitha Chikati <srechi453.fusdk12@gmail.com>
2022-03-03 21:46:00 -0800

    version 1 testing

--- scrape.go
+++ scrape.go
@@ -6,18 +6,21 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type NewReleases struct {
+	Name string
+}
+
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
 	// Instantiate default collector
+	namesData := make([]NewReleases, 0)
+	var names []string
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	c.OnHTML("div.tab_item_name", func(e *colly.HTMLElement) {
+		names := append(names, e.Text)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +29,8 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://store.steampowered.com/explore/new/")
+	for i := 0; i < len(names); i++ {
+		namesData := NewReleases{Name: names[i]}
+	}
 }
